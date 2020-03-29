261a817a119db4fbd6a1128c30735d52a9bdd2bb
alvin <mr.alvinaquino@gmail.com>
2020-03-29 15:40:16 +0000

    added kill session to one session mode reworked main loop and refactored

--- tmuxrc.sh
+++ tmuxrc.sh
@@ -7,7 +7,7 @@ function killmode () {
 	while true; do
 		sessions=`tmux list-sessions`
 		if [ -z "$sessions" ]; then
-			exit  1
+			return 1
 		fi
 
 		echo "$sessions"
@@ -18,18 +18,11 @@ function killmode () {
 			exit 0
 		else
 			tmux kill-session -t $option
-
 		fi
 	done
 }
 
-sessions=`tmux list-sessions`
-if [ ! -z "$sessions" ]; then
-	count=`tmux list-sessions | grep -c '^'`
-	echo "$count tmux session(s) found!"
-	echo "$sessions"
-	if [ $count -eq 1 ]; then
-		while true; do
+function one_session() {
 			echo "[enter] last session | [n]ew session | e[x]it to shell"
 			read -sn1 option
 			if [ "$option" == "n" ]; then
@@ -37,13 +30,15 @@ if [ ! -z "$sessions" ]; then
 				exit 1
 			elif [ -z "$option" ]; then
 				tmux a
+			elif [ "$option" == "k" ]; then
+                tmux kill-session
 			elif [ "$option" == "x" ]; then
 				echo ""
 				exit 0
 			fi
-		done
-	else
-		while true; do
+}
+
+function multi_session() {
 			echo "[enter] last session | [n]ew session | e[x]it to shell | [k]ill a session"
 			echo -n ": "
 			read option
@@ -52,27 +47,41 @@ if [ ! -z "$sessions" ]; then
 				exit 1
 			elif [ "$option" == "k" ]; then
 				if ! killmode; then
-					exit 1
+					return 1
 				fi
 			elif [ "$option" != "x" ]; then
 				tmux attach-session -t "$option"
+                exit 0
 			else
 				exit 0
 			fi
-		done
-	fi
-else
+}
+
+function no_session () {
 	echo "No tmux sessions found. "
-	
-	while true; do
 		echo "[enter] new session | e[x]it to regular shell"
 		read -sn1 option
 		if [ -z "$option" ]; then
 			tmux new
+            exit 0
 		elif [ "$option" == "x" ]; then
 			echo ""
-			exit 1
+			exit 0
 		fi
+}
 
-	done
+while true; do
+sessions=`tmux list-sessions`
+if [ ! -z "$sessions" ]; then
+	count=`tmux list-sessions | grep -c '^'`
+	echo "$count tmux session(s) found!"
+	echo "$sessions"
+	if [ $count -eq 1 ]; then
+        one_session
+    else
+        multi_session
+	fi
+else
+    no_session
 fi
+done
